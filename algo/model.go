ce734e7c18695216a75a367d2a58394d2723c0e5
BenJoyenConseil <b.joyenconseil@gmail.com>
2019-12-11 22:14:41 +0100

    remove Predictor interface

--- algo/model.go
+++ algo/model.go
@@ -4,11 +4,7 @@ import "gonum.org/v1/gonum/mat"
 
 // Model is an abstraction of estimators provided in the ensemble package
 type Model interface {
-	Predictor
+	Predict(m *mat.Dense) (predictions []float64)
 	IsFitted() bool
 	PredictRow(v mat.Vector) float64
-}
-
-type Predictor interface {
-	Predict(m *mat.Dense) (predictions []float64)
 }
