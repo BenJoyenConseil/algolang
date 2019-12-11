04f2d68879c8941852ce818c0587b70e117c7521
BenJoyenConseil <b.joyenconseil@gmail.com>
2019-12-11 22:11:11 +0100

    move model to algo/model and implemente it in decision/tree

--- algo/model.go
+++ algo/model.go
@@ -1,10 +1,14 @@
-package rf
+package algo
 
 import "gonum.org/v1/gonum/mat"
 
 // Model is an abstraction of estimators provided in the ensemble package
 type Model interface {
+	Predictor
 	IsFitted() bool
-	Predict(m *mat.Dense) (predictions []float64)
 	PredictRow(v mat.Vector) float64
+}
+
+type Predictor interface {
+	Predict(m *mat.Dense) (predictions []float64)
 }
